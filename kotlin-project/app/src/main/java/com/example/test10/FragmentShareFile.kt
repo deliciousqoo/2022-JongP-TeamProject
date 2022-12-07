eb5ded91f6d54396175f4634f7c35ace41c49548
milouw <milouw56@gmail.com>
2022-12-08 00:05:50 +0900

    FragmentShareFile / implement share file + add connect with manage main page

--- kotlin-project/app/src/main/java/com/example/test10/FragmentShareFile.kt
+++ kotlin-project/app/src/main/java/com/example/test10/FragmentShareFile.kt
@@ -1,5 +1,6 @@
 package com.example.test10
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -20,18 +21,18 @@ class FragmentShareFile : Fragment(),MainActivity.onBackPressedListener{
         }
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentShareFileBinding.inflate(inflater, container, false)
-        super.onViewCreated(requireView(), savedInstanceState)
         val webView: WebView = binding.webView
-        val settings = webView.settings
-        settings.javaScriptEnabled = true
+        webView.settings.javaScriptEnabled = true
+        val url = "https://docs.google.com/presentation/d/1F2ah2MX1SsLtYB-isxRNNXQfI0-73WMg/edit?usp=sharing&ouid=107207103656101966670&rtpof=true&sd=true"
         webView.webViewClient = WebViewClient()
-        webView.loadUrl("http://www.google.com")
+        webView.loadUrl(url)
         return binding.root
     }
 
